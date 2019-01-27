dbd85679c0555c9bd55639d2ad77001da5275b2f
HoldenChen <chrhaiyang@163.com>
2019-01-27 20:16:43 +0800

    add bookid info to props.

--- src/component/BookDetail.js
+++ src/component/BookDetail.js
@@ -4,18 +4,18 @@ import SelectShelf from './SelectShelf'
 class BookDetail extends Component{
 
     render(){
-        const {id,name,author,shelf,imgUrl} = this.props
+        const {id,name,author,shelf,imgUrl} = this.props;
         const divStyle = {
             width: '128px',
             height: '193px',
             backgroundImage: `url(${imgUrl})`
-        }
+        };
         return(
             <div className='book'>
                 <div className='book-top'>
                     <div className ='book-cover' style={divStyle}>
                         <div className='book-shelf-changer'>
-                           <SelectShelf defaultvalue ={shelf}/>
+                           <SelectShelf defaultvalue ={shelf} bookid = {id}/>
                         </div>
                     </div>
                 </div>
